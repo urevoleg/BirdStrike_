bf5331cefb7943f2ad7068e9a77b0805c5a93b57
YarRus12 <yaroslav.russu@mail.ru>
2023-09-09 22:10:26 +1000

    Скрипт разбит на 2 ДАГа Дампы подготовлены

---
+++ main_dds.py
@@ -0,0 +1,39 @@
+import os
+import logging
+import datetime
+from config import Config
+from modules.dds_loader import DdsControler
+from modules.cdm_loader import CdmControler
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+config = Config()
+log = logging.getLogger(__name__)
+
+dds_uploads = DdsControler(date=datetime.datetime.now().date(),
+                           pg_connect=config.pg_warehouse_db(),
+                           schema='DDS',
+                           logger=log)
+cdm_uploads = CdmControler(date=datetime.datetime.now().date(),
+                           pg_connect=config.pg_warehouse_db(),
+                           schema='CDM',
+                           logger=log)
+
+with DAG(
+        dag_id="example_timetable_dag",
+        start_date=datetime.datetime(2018, 1, 1),
+        max_active_runs=1,
+        schedule="5 4 * * 3",
+        catchup=False,
+        default_args={
+            "retries": 1,
+            "retry_delay": datetime.timedelta(minutes=3)}
+) as dag:
+    task_animal_incidents = PythonOperator(
+        task_id='download_animal_incidents',
+        python_callable=dds_uploads.upload_aircraft_incidents,
+        op_kwargs={'table_name': 'aircraft_incidents'})
+    task_weather_data = PythonOperator(
+        task_id='download_weather_data',
+        python_callable=dds_uploads.upload_weather_observation,
+        op_kwargs={'table_name': 'weather_observation'})
